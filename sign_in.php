545c123f4df4f4f271d7a60aee2f3e63a648b9a5
Shrinath Korajkar <shrinathkorajkar@gmail.com>
2022-01-12 20:49:38 +0530

    error fixing

--- sign_in.php
+++ sign_in.php
@@ -73,28 +73,23 @@
       $emergency = $_POST["emergency"];
 
 
-      // $sql1 = "INSERT INTO `user` (`AADHAR_NO`, `USERNAME`) VALUES ('$aadhaar', '$name');";
-      $sql = "INSERT INTO `p_details` (`AADHAR_NO`, `AGE`, `BLOOD_GRP`, `DOB`, `CONTACT_NO`, `ECONTACT_NO`, `GENDER`) VALUES ('$aadhaar', '$age', '$bloodgroup', '$birthdayDate', '$phoneNumber', '$emergency', '$gender');
-          INSERT INTO `user` (`AADHAR_NO`, `USERNAME`) VALUES ('$aadhaar', '$name')";
+      $sql1 = "INSERT INTO `user` (`AADHAR_NO`, `USERNAME`) VALUES ('$aadhaar', '$name');";
+      $sql = "INSERT INTO `p_details` (`AADHAR_NO`, `AGE`, `BLOOD_GRP`, `DOB`, `CONTACT_NO`, `ECONTACT_NO`, `GENDER`) VALUES ('$aadhaar', '$age', '$bloodgroup', '$birthdayDate', '$phoneNumber', '$emergency', '$gender');";
       $result = mysqli_query($conn, $sql);
-      // $result1 = mysqli_query($conn, $sql1);
-      if ($result1) {
+      $result1 = mysqli_query($conn, $sql1);
+      if ($result && $result1) {
         $showAlert = true;
       }
       if ($showAlert) {
-        echo ' <div class="alert alert-Success alert-dismissible fade show" role="alert">
-      <strong>Success!</strong> Regestered Successfully
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-      </button>
-      </div> ';
+        echo ' <div class="alert alert-success alert-dismissible fade show" role="alert">
+        <strong>Success!</strong> Regestered Successfully
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>';
       } else {
         echo ' <div class="alert alert-warning alert-dismissible fade show" role="alert">
-      <strong>Error!</strong> Registration Failed Try Once Again
-      <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-      </button>
-      </div> ';
+        <strong>Error!</strong> Registration Failed Try Once Again
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div> ';
       }
     }
     ?>
